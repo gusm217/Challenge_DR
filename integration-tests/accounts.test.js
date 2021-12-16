e255a2a8bd8dd3fa5027df7cf07f66187205159f
Gustavo Mendes <gusm217@gmail.com>
2021-12-16 20:05:55 -0300

    Implementa testes para o erro no cadastro de uma nova conta com mesmo CPF

--- integration-tests/accounts.test.js
+++ integration-tests/accounts.test.js
@@ -5,10 +5,13 @@ const { MongoClient, ObjectId } = require('mongodb');
 const { getConnection } = require('./connectionMock');
 const app = require('../index');
 const { describe } = require('mocha');
+const { object } = require('joi');
 
 const { expect } = chai;
 
 chai.use(chaiHttp);
+
+const VALID_ID = '617b6a525c769ab68b4036a0';
 
 describe('Testes para a rota /accconunts', () => {
   let connectionMock;
@@ -35,13 +38,43 @@ describe('Testes para a rota /accconunts', () => {
       MongoClient.connect.resolves(connectionMock);
     });
 
-    it('Deve receber um código HTTP 500', function() {
+    it('deve receber um código HTTP 500', function() {
       expect(response).to.have.status(500);
       });
     
-    it('Deve receber um objeto de erro', function() {
+    it('deve receber um objeto de erro', function() {
       expect(response.body).to.be.an('object');
     });
   });
+
+  describe('Testes para cadastro de uma nova conta', function () {
+    describe('Quando já existe uma conta cadastrada no CPF', function() {
+      let response;
+
+      before(async function() {
+        await connectionMock.db('DR_Challenge').collection('accounts').insertOne({
+          _id: ObjectId(VALID_ID),
+          nome: 'Fulano de Tal',
+          cpf: '12345678901',
+      });
+
+      response = await chai.request(app).post('/accounts').send({
+        nome: 'Sicrano de tal',
+        cpf: '12345678901',
+      });
+
+      it('deve receber um código 400', function() {
+        expect(response).to.have.status(400);
+      });
+
+      it('deve receber um objeto de erro', function() {
+        expect(response.body).to.be.an('object');
+      });
+
+      it('deve receber a mensagem de erro correta', function() {
+        expect(response.body.message).to.be.equal('CPF já cadastrado');
+      });
+    })
+  })
 });
  
